76656a6ae1690a09cf7c3c2d8150c4337e1e36e9
steve hanlon <rshanlon@gmail.com>
2018-12-14 17:36:43 -0800

    Refactored App convertTemp function into 2 functions; adjusted variables in App.js; refactored TempBar component; adjusted bits of CSS

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import TemperatureBar from '../TemperatureBar/TemperatureBar';
 import { ResultsArea } from '../ResultsArea/ResultsArea';
@@ -7,7 +7,7 @@ import cool from '../ResultsArea/images/Willamette_Olympics_desktop.jpg';
 import warm from '../ResultsArea/images/cactusJuggling_Desktop.jpg';
 import hot from '../ResultsArea/images/Arabian_Olympics_desktop.jpg';
 
-class App extends React.Component {
+class App extends Component {
 
     state = {
       fahrenheit: null,
@@ -17,40 +17,49 @@ class App extends React.Component {
       picture: ""
     }
 
-  // convertTemp returns F and C temps which then builds the total response
+  // convertTemp returns F and C temps which then sets state and calls picActivtyResponse func
   convertTemp = (kelvin) => {
     // Celsius is 273 degrees less than Kelvin
-    const c = this.celsius = kelvin - 273;
-    // Solve for Fahrenheit based on Celsius; round down with Math library
-    const f = this.fahrenheit = Math.floor(c * (9/5) + 32);
+    let celsius = kelvin - 273;
+    // Solve for Fahrenheit based on Celsius then round down
+    let fahrenheit = Math.floor(celsius * (9/5) + 32);
     // Make the answer based on temperatures
-    this.answer = `${kelvin}°K = ${f}°F or ${c}°C`;
-    // Alert if text input is empty and button is clicked
+    let answer = `${kelvin}°K = ${fahrenheit}°F or ${celsius}°C`;
+    // Alert if button is clicked but text input is empty
     if (kelvin === 'undefined' || kelvin === null) {
         alert("I pity the fool who just clicks the button. Enter a Kelvin temperature!");
         return false;
     }
-    // Set the activity and corresponding pic based on temperatures
-    if (f < 50) {
-      this.activity = "Prepare for Antartic Winter olympics!";
-      this.picture = <img alt="Antarctica" src={cold} />;
-    } else if (f < 90) {
-      this.activity = "Time to go kayaking on the Willamette!";
-      this.picture = <img alt="Willamette" src={cool} />;
-    } else if (f < 120) {
-      this.activity = "Prepare for Las Vegan Summer Olympics Cactus Juggling Event!";
-      this.picture = <img alt="Cactus Juggling" src={warm} />;
-    } else {
-      this.activity = "Prepare for Saudi Arabian Summer Olympics Dirtboarding Event!";
-      this.picture = <img alt="Dirtboarding" src={hot} />;
-    }
-    // set state for these properties
+
     this.setState({
-      celsius: this.c,
-      fahrenheit: this.f,
-      answer: this.answer,
-      activity: this.activity,
-      picture: this.picture
+        celsius,
+        fahrenheit,
+        answer,
+      }, this.picActivtyResponse
+    );
+  }
+
+  picActivtyResponse = () => {
+    let activity;
+    let picture;
+    // Set the activity and corresponding pic based on temperatures
+    if (this.state.fahrenheit < 50) {
+      activity = "Prepare for Antartic Winter Olympics!";
+      picture = <img alt="Antarctica" src={cold} />;
+    } else if (this.state.fahrenheit < 90) {
+      activity = "Time to go kayaking on the Willamette!";
+      picture = <img alt="Willamette" src={cool} />;
+    } else if (this.state.fahrenheit < 120) {
+      activity = "Prepare for Las Vegan Summer Olympics Cactus Juggling event!";
+      picture = <img alt="Cactus Juggling" src={warm} />;
+    } else {
+      activity = "Prepare for Saudi Arabian Summer Olympics Dirtboarding event!";
+      picture = <img alt="Dirtboarding" src={hot} />;
+    }
+
+    this.setState({
+      activity,
+      picture
     });
   }
 
@@ -60,22 +69,22 @@ class App extends React.Component {
   render() {
     const { fahrenheit, celsius, answer, activity, picture } = this.state;
     return (
-      <div className="App">
+      <main className="App">
         <header className="NavBar">
           <h1>Kelvin Konverter</h1>
         </header>
-          <TemperatureBar convert={this.convertTemp} />
-          <ResultsArea
-            fahrDegrees={fahrenheit}
-            celsDegrees={celsius}
-            answer={answer}
-            activity={activity}
-            pic={picture}
-          />
-          <footer className="Footer">
-            <h3>2018©️SteveHanlon</h3>
-          </footer>
-      </div>
+        <TemperatureBar convert={this.convertTemp} />
+        <ResultsArea
+          fahrDegrees={fahrenheit}
+          celsDegrees={celsius}
+          answer={answer}
+          activity={activity}
+          pic={picture}
+        />
+        <footer className="Footer">
+          <h3>2018©️SteveHanlon</h3>
+        </footer>
+      </main>
     );
   }
 }
--- src/components/TemperatureBar/TemperatureBar.js
+++ src/components/TemperatureBar/TemperatureBar.js
@@ -1,36 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './TemperatureBar.css';
 
 // TemperatureBar receives convertTemp method for onChange handler to set state of Kelvin
 // and props for onClick event
-class TemperatureBar extends React.Component {
-  state = {
-    k_number: null
-  }
+class TemperatureBar extends Component {
 
-  convert = () => {
-    this.props.convert(this.state.k_number);
+  state = {
+    kelvin_temp: null
   }
 
   handleInputEvent = (event) => {
       this.setState({
-        k_number: event.target.value
+        kelvin_temp: event.target.value
       });
   }
 
   render() {
+    const { kelvin_temp } = this.state;
     return (
       <section id="TempBar" className="TempBar">
         <input type="text" placeholder="Enter a Kelvin temperature." onChange={this.handleInputEvent} />
-        <button id="konvertBtn" onClick={this.convert}>Konvert</button>
+        <button id="konvertBtn" onClick={() => this.props.convert(kelvin_temp)}>Konvert</button>
       </section>
     );
   }
 }
 
 TemperatureBar.PropTypes = {
-  convert: PropTypes.func.isRequired
+  convert: PropTypes.func.isRequired,
+  handleInputEvent: PropTypes.func.isRequired
 }
 
 export default TemperatureBar;
